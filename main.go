d11d8c5516170d7dd223f666dadefb516a9ee1b8
Reinholds Zviedris <reinholds@zviedris.lv>
2020-05-14 02:47:39 +0300

    go fmt main.go

--- main.go
+++ main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
+	"github.com/Estivador/terraform-provider-hdns/hdns"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/plugin"
 	"github.com/hashicorp/terraform-plugin-sdk/terraform"
-	"github.com/Estivador/terraform-provider-hdns/hdns"
-        "github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 )
 
 func main() {
